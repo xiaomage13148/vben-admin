c275ef3adaaa8c1a356d9124cdf9dd137641d522
mjh <majianghai577@gmail.com>
2024-04-25 11:01:42 +0800

    docs: 添加指令注释

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -9,7 +9,8 @@ import { setupEllipsisDirective } from './ellipsis';
 export function setupGlobDirectives(app: App) {
   // 设置权限指令
   setupPermissionDirective(app);
-  //
+  // 设置正在加载指令
   setupLoadingDirective(app);
+  // 设置省略指令
   setupEllipsisDirective(app);
 }
